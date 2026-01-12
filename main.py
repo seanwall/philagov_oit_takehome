344188d098ec43757348f241b709b9a1aafcebea
Sean Wallace <wallace.se@husky.neu.edu>
2026-01-12 17:33:54 +0000

    Query 2025 violations

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import requests
 import sqlite3
 import sqlite_utils
 
-PHL_API_URL = "https://phl.carto.com/api/v2"
+PHL_CARTO_URL = "https://phl.carto.com/api/v2"
+PHL_API_URL = "https://api.phila.gov/"
 
 PUBLIC_CASES_TABLE = "public_cases_fc"
 VIOLATIONS_TABLE = "violations"
@@ -12,12 +13,13 @@ def get(url):
     resp = requests.get(url)
     # TODO status code handling
     # TODO pagination?
-    print(resp.status_code)
+    if resp.status_code != 200:
+        print(resp.json())
     return resp.json()
 
-def query_phl(db, table, query):
-    # Queries Phila Gov carto api and writes the output into the given table in the provided sqlite db.
-    url = f"{PHL_API_URL}/sql?q={query}"
+def query_phl_carto(db, table, query):
+    # Queries Phila Gov carto api and writes the output into the specified table in the provided sqlite db.
+    url = f"{PHL_CARTO_URL}/sql?q={query}"
     json_resp = get(url)
     
     # sqlite_utils expects just a list of rows
@@ -37,8 +39,11 @@ if __name__ == "__main__":
 
     db = sqlite_utils.Database("philagov.db")
     # Query 311 tickets and insert into 'public_cases_fc' table in our SQLite DB
-    # Using the clause 'agency_responsible LIKE License%' in lieu of 'agency_responsible='License & Inspections' since the later seemed to 
-    # cause problems with how the carto API parses SQL queries passed as query params in the URL.
-    query_phl(db, "public_cases_fc", "SELECT service_request_id, address, agency_responsible, requested_datetime FROM public_cases_fc WHERE requested_datetime>='2025-01-01' AND requested_datetime<'2026-01-01' AND agency_responsible LIKE 'License%';")
+    # NB: Using the clause 'agency_responsible LIKE License%' in lieu of 'agency_responsible='License & Inspections' since the later seemed to 
+    # cause problems with how the carto API parses SQL queries passed as query params in the URL. This is not ideal, but there aren't any agencies
+    # other than License & Inspections that match, so I'm fine with it for the sake of this takehome.
+    query_phl_carto(db, "public_cases_fc", "SELECT service_request_id, address, agency_responsible, requested_datetime FROM public_cases_fc WHERE requested_datetime>='2025-01-01' AND requested_datetime<'2026-01-01' AND agency_responsible LIKE 'License%';")
     # Query Licenses & Inspections violations
+    query_phl_carto(db, "violations", "SELECT casenumber, opa_account_num, address, casestatus, violationdate, casecreateddate, casecompleteddate FROM violations WHERE casecreateddate>='2025-01-01' AND casecreateddate<'2026-01-01'")
     # Fetch Address -> opa_account_num from AIS API
+    # print(get(f"{PHL_API_URL}/ais/v2/search/"))
