2254135e91186b3a7ebf070413e1929430c83ac9
Sean Wallace <wallace.se@husky.neu.edu>
2026-01-12 16:59:08 +0000

    Query 2025 L&I 311 tickets

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import requests
 import sqlite3
 import sqlite_utils
@@ -9,20 +10,35 @@ VIOLATIONS_TABLE = "violations"
 
 def get(url):
     resp = requests.get(url)
+    # TODO status code handling
+    # TODO pagination?
     print(resp.status_code)
     return resp.json()
 
-def query_phl(query):
-    # Queries Phila Gov carto api and pipes the output into a sqlite db.
+def query_phl(db, table, query):
+    # Queries Phila Gov carto api and writes the output into the given table in the provided sqlite db.
     url = f"{PHL_API_URL}/sql?q={query}"
     json_resp = get(url)
-    print(json_resp)
-
-    db = sqlite_utils.Database("test.db")
-    db["test"].insert_all(json_resp["rows"])
+    
+    # sqlite_utils expects just a list of rows
+    rows = json_resp["rows"]
+    db[table].insert_all(rows)
     return True
 
 
 
 if __name__ == "__main__":
-    query_phl("SELECT * FROM public_cases_fc LIMIT 1")
+    # Create a sqlite DB to hold all our fetched data
+    db_filename = "philagov.db"
+
+    if os.path.isfile(db_filename):
+        os.remove(db_filename)
+        print("removed db file")
+
+    db = sqlite_utils.Database("philagov.db")
+    # Query 311 tickets and insert into 'public_cases_fc' table in our SQLite DB
+    # Using the clause 'agency_responsible LIKE License%' in lieu of 'agency_responsible='License & Inspections' since the later seemed to 
+    # cause problems with how the carto API parses SQL queries passed as query params in the URL.
+    query_phl(db, "public_cases_fc", "SELECT service_request_id, address, agency_responsible, requested_datetime FROM public_cases_fc WHERE requested_datetime>='2025-01-01' AND requested_datetime<'2026-01-01' AND agency_responsible LIKE 'License%';")
+    # Query Licenses & Inspections violations
+    # Fetch Address -> opa_account_num from AIS API
