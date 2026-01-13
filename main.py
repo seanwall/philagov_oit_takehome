42751654a438ccf1ce35074dce7dd26d67bcc352
Sean Wallace <wallace.se@husky.neu.edu>
2026-01-13 17:33:39 +0000

    Threadpool approach for fetching opa_account_num from AIS API

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import requests
 import sqlite3
 import sqlite_utils
+import concurrent.futures
 
 PHL_CARTO_URL = "https://phl.carto.com/api/v2"
 PHL_API_URL = "https://api.phila.gov/"
@@ -11,10 +12,6 @@ VIOLATIONS_TABLE = "violations"
 
 def get(url):
     resp = requests.get(url)
-    # TODO status code handling
-    # TODO pagination?
-    if resp.status_code != 200:
-        print(resp.json())
     return resp
 
 def query_phl_carto(db, table, query):
@@ -28,59 +25,76 @@ def query_phl_carto(db, table, query):
     return True
 
 def fetch_opa_account_nums(addresses):
-    # Given list of addresses, fetches opa_account_num from AIS API
-    num_addresses = len(addresses)
-    address_opa_records = []
-    ct = 0
-    for address in addresses:
-        ct += 1
-        resp = get(f"{PHL_API_URL}/ais/v2/search/{address}")
+    # Given list of addresses, fetches opa_account_num from AIS API using a python thread pool
+    # https://docs.python.org/3/library/concurrent.futures.html
+
+    # Task target for thread pool - sends request to Philly AIS API and grabs opa_account_num
+    def get_opa_account_num(address):
+        resp = get(f"{PHL_API_URL}/ais/v2/search/{address}?opa_only")
         if resp.status_code == 404:
             # Nothing found in AIS for address, just move to next address
-            continue
-        json_resp = resp.json()
-        for feature in json_resp['features']:
-            if feature['ais_feature_type'] == "address" and feature['match_type'] == 'exact':
-                properties = feature['properties']
-                ais_address = properties['street_address']
-                opa_account_num = properties['opa_account_num']
-                if address != ais_address:
-                    print(f"Something weird; 311 Address: {address}, AIS address: {ais_address}, OPA num: {opa_account_num}")
-                address_opa_records += [{"address": address, "opa_account_num": opa_account_num}]
-                if ct % 100 == 0:
-                    print(f"Found {ct} addresses out of {num_addresses}")
+            return False
+        else:
+            return resp
+    
 
-    return address_opa_records
+    address_to_opa_account_num_records = []
+    total = len(addresses)
+    ct = 0
+    with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
+        # Submit addresses to thread pool
+        future_to_address = {executor.submit(get_opa_account_num, address): address for address in addresses}
+        
+        # Process completed tasks
+        for future in concurrent.futures.as_completed(future_to_address):
+            address = future_to_address[future]
+            try:
+                # Progress tracking
+                if ct % 500 == 0:
+                    print(f"opa_account_num fetch progress: {ct}/{total}")
+                ct += 1
+
+                # Process result
+                result = future.result()
+                if result:
+                    json_resp = result.json()
+                    # Grab opa_account_num and put it in a record ready to be inserted into DB
+                    for feature in json_resp['features']:
+                        if feature['ais_feature_type'] == "address" and feature['match_type'] == 'exact':
+                            opa_account_num = feature['properties']['opa_account_num']
+                            address_to_opa_account_num_records += [{"address": address, "opa_account_num": opa_account_num}]
+            except Exception as e:
+                print(f"Exception in threadpool worker: {e}")
+    
+    return address_to_opa_account_num_records
 
 if __name__ == "__main__":
     # Create a sqlite DB to hold all our fetched data
     db_filename = "philagov.db"
-
-    # if os.path.isfile(db_filename):
-    #     os.remove(db_filename)
-    #     print("removed db file")
+    if os.path.isfile(db_filename):
+        os.remove(db_filename)
+        print("removed db file")
     db = sqlite_utils.Database(db_filename)
+
     # Query 311 tickets and insert into 'public_cases_fc' table in our SQLite DB
     # NB: Using the clause 'agency_responsible LIKE License%' in lieu of 'agency_responsible='License & Inspections' since the later seemed to 
     #     cause problems with how the carto API parses SQL queries passed as query params in the URL. This is not ideal, but there aren't any agencies
     #     other than License & Inspections that match, so I'm fine with it for the sake of this takehome.
-   #query_phl_carto(db, "public_cases_fc", "SELECT service_request_id, address, agency_responsible, requested_datetime FROM public_cases_fc WHERE requested_datetime>='2026-01-01' AND requested_datetime<'2027-01-01' AND agency_responsible LIKE 'License%' AND address IS NOT NULL AND address NOT IN ('-','.');")
-    
+    print("Fetching 311 Tickets...")
+    query_phl_carto(db, "public_cases_fc", "SELECT service_request_id, address, agency_responsible, status, requested_datetime, closed_datetime FROM public_cases_fc WHERE requested_datetime>='2025-01-01' AND requested_datetime<'2026-01-01' AND agency_responsible LIKE 'License%' AND address IS NOT NULL AND address NOT IN ('-','.');")
+
     # Query Licenses & Inspections violations
-    # NB: I noticed there were rows with duplicate casenumbers in the violations dataset, which I'm not sure how to interpret. 
-    #     Grouping this query by (casenumber, opa_account_num, address, casestatus) to eliminate the duplicates, and
-    #     taking the min of the datetime fields - this likely results in fudging those fields across rows but accuracy of those fields isn't relevant for the required analysis.
-    #query_phl_carto(db, "violations", "SELECT casenumber, opa_account_num, address, casestatus, min(violationdate) as violationdate, min(casecreateddate) as casecreateddate, min(casecompleteddate) as casecompleteddate FROM violations WHERE casecreateddate>='2026-01-01' AND casecreateddate<'2027-01-01' GROUP BY casenumber, opa_account_num, address, casestatus")
-    
-    # Fetch Address -> opa_account_num from AIS API
+    print("Fetching Violations...")
+    query_phl_carto(db, "violations", "SELECT casenumber, opa_account_num, address, casestatus, violationdate, casecreateddate, casecompleteddate FROM violations WHERE casecreateddate>='2025-01-01' AND casecreateddate<'2026-01-01'")
+
 
     # Get unique addresses from public cases dataset
     con = sqlite3.connect("philagov.db")
     cur = con.cursor()
     res = cur.execute('SELECT DISTINCT address FROM public_cases_fc;')
     addresses = [row[0] for row in res.fetchall()]
-    a2 = addresses[0:10]
-    address_opa_records = fetch_opa_account_nums(a2)
-    
-    db["address_to_opa_account_num"].insert_all(address_opa_records)
 
+    # Fetch Address -> opa_account_num from AIS API
+    print("Fetching opa_account_nums...")
+    address_opa_records = fetch_opa_account_nums(addresses)
+    db["address_to_opa_account_num"].insert_all(address_opa_records)
